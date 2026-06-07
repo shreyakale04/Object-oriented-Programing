17639e95da89b0ff5385bd5488685e25a0d0a005
shreyakale08 <kaleshreya347@gmail.com>
2026-06-07 16:41:42 +0530

    Find Amount of water in container

---
+++ src/com/containerEx/Water.java
@@ -0,0 +1,38 @@
+package com.containerEx;
+
+import example.obj.Main;
+
+public class Water {
+
+	static int maxWater(int[]arr){
+	
+		int res = 0;
+		int n = arr.length;
+		
+		for(int i = 0; i<n; i++){
+			for(int j = i+1; j<n; j++){
+				
+			int amount = Math.min(arr[i], arr[j]) * (j-i); // 2,1= 1 *1-2 //
+			res = Math.max(amount, res);
+				
+				
+}
+		
+		    
+		}
+		 return res;
+		
+	}
+	public static void main(String[] args){
+		
+		int[]arr = {2, 1, 8, 6, 4, 6, 5, 5};
+		System.out.println(maxWater(arr));
+		
+		
+		
+		}
+		
+	}
+	
+
+
